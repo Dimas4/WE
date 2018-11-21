ec5dcaa8f3194857fe94960f49f536d7e9721a1a
Ivan <ivanshatukho@yandex.ru>
2018-11-21 10:32:53 +0300

    Docker-compose completed

--- utils/celery/celery.py
+++ utils/celery/celery.py
@@ -1,6 +1,6 @@
 import json
 
-from app.database.model import db, EmailsS
+from app.database.model import db, Emails
 from utils.crawler.crawler import Crawler
 from .create_celery import celery
 
@@ -10,6 +10,7 @@ def start_search(url, token):
     crawler = Crawler(url)
     answer = json.dumps(crawler.search())
 
-    emails = EmailsS(token=token, answer=answer)
+    emails = Emails(token=token, answer=answer)
     db.session.add(emails)
     db.session.commit()
+    print(Emails.get_one_by_token(token))
